1ac7bb43226d610b85054d5ed2a7c2a0f86e2e74
Tulio Ruiz <tulio@vauxoo.com>
2014-09-17 19:01:30 -0500

    [IMP] Exception handling for database connections

--- files/config_db.py
+++ files/config_db.py
@@ -2,12 +2,25 @@
 
 import psycopg2
 
-conn = psycopg2.connect("dbname='postgres'")
+try:
+    conn = psycopg2.connect("dbname='postgres'")
+except:
+    print "Could not connect to PostgreSQL server"
+    raise
 conn.set_isolation_level(0)
 cursor = conn.cursor()
-cursor.execute("CREATE USER docker WITH SUPERUSER PASSWORD 'docker';")
-cursor.execute("CREATE USER odoo WITH CREATEDB PASSWORD 'odoo';")
-cursor.execute("CREATE DATABASE docker ENCODING 'utf8' OWNER docker TEMPLATE template1 ")
-conn.commit()
+try:
+    print "Creating users"
+    cursor.execute("CREATE USER docker WITH SUPERUSER PASSWORD 'docker';")
+    cursor.execute("CREATE USER odoo WITH CREATEDB PASSWORD 'odoo';")
+except:
+    print "Could not create users"
+    raise
+
+try:
+    cursor.execute("CREATE DATABASE docker ENCODING 'utf8' OWNER docker TEMPLATE template1 ")
+except:
+    print "Could not create docker database"
+    raise
 cursor.close()
 conn.close()
