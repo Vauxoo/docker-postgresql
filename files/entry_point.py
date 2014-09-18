e2a606e5dfe172caedfb0a5a0df055a013d85b57
Tulio Ruiz <tulio@vauxoo.com>
2014-09-17 20:18:19 -0500

    [FIX] Some pep8 issues

--- files/entry_point.py
+++ files/entry_point.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
-from  os import listdir, stat, chown
+from os import listdir, stat, chown
 from subprocess import Popen, call
 import pwd
 
 postgres_path = '/var/lib/postgresql'
 st = stat(postgres_path)
 owner = pwd.getpwuid(st.st_uid).pw_name
-if owner!="postgres":
+if owner != "postgres":
     print "Owner %s" % owner
     call(["chown", "-R", "postgres", postgres_path])
 
-if listdir(postgres_path) == []: 
+if listdir(postgres_path) == []:
     call(['pg_createcluster', '9.3', 'main', '-e=utf8'])
     with open("/etc/postgresql/9.3/main/pg_hba.conf", "a") as pg_hba:
         pg_hba.write("host all  all    0.0.0.0/0  md5")
