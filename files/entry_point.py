0c28493c2b43a020b5a25f44bae94ca4c6a026fc
Tulio Ruiz <tulio@vauxoo.com>
2014-10-24 18:29:40 -0500

    [IMP] Some refactoring, check config folder permission

--- files/entry_point.py
+++ files/entry_point.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python
 
-from os import listdir, stat, chown
+from os import listdir, stat, chown, path
 from subprocess import Popen, call
 import pwd
 
-postgres_path = '/var/lib/postgresql'
-st = stat(postgres_path)
-try:
-    owner = pwd.getpwuid(st.st_uid).pw_name
-except KeyError:
-    owner = "None"
-if owner != "postgres":
-    print "Owner %s" % owner
-    call(["chown", "-R", "postgres", postgres_path])
+POSTGRES_PATH = '/var/lib/postgresql'
+POSTGRES_CONFIG = '/etc/postgresql'
 
-if listdir(postgres_path) == []:
-    call(['pg_createcluster', '9.3', 'main', '-e=utf8'])
-    with open("/etc/postgresql/9.3/main/pg_hba.conf", "a") as pg_hba:
-        pg_hba.write("host all  all    0.0.0.0/0  md5")
-    with open("/etc/postgresql/9.3/main/postgresql.conf", "a") as pg_config:
-        pg_config.write("listen_addresses='*'")
-    call(["/etc/init.d/postgresql", "start"])
-    call(["su", "postgres", "-c /config_db.py"])
-    call(["/etc/init.d/postgresql", "stop"])
-call(["/usr/bin/supervisord"])
+def get_owner(file_name):
+    '''
+    This function gets owner name from system for a directory or file
+
+    :param str file_name: File or directory name
+    :returns: Owner name
+    '''
+    file_stat = stat(file_name)
+    try:
+        owner = pwd.getpwuid(file_stat.st_uid).pw_name
+    except KeyError:
+        owner = "None"
+    return owner
+
+def main():
+    '''
+    Main entry point function
+    '''
+    if "postgres" != get_owner(POSTGRES_CONFIG):
+        call(["chown", "-R", "postgres", POSTGRES_CONFIG])
+
+    if "postgres" != get_owner(POSTGRES_PATH):
+        call(["chown", "-R", "postgres", POSTGRES_PATH])
+
+    if listdir(POSTGRES_PATH) == []:
+        call(['pg_createcluster', '9.3', 'main', '-e=utf8'])
+        with open(path.join(POSTGRES_CONFIG, "9.3", "main", "pg_hba.conf"), "a") as pg_hba:
+            pg_hba.write("host all  all    0.0.0.0/0  md5")
+        with open(path.join(POSTGRES_CONFIG, "9.3", "main", "postgresql.conf"), "a") as pg_config:
+            pg_config.write("listen_addresses='*'")
+        call(["/etc/init.d/postgresql", "start"])
+        call(["su", "postgres", "-c /config_db.py"])
+        call(["/etc/init.d/postgresql", "stop"])
+    call(["/usr/bin/supervisord"])
+
+if __name__ == '__main__':
+    main()
